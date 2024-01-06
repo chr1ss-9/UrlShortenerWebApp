ebafeaccde8ed65405b0fb9dd8a5d74b8b521ad7
Chriss <christian_904@outlook.com>
2024-01-06 12:04:32 +0100

    Created project.

---
+++ UrlShortenerWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace UrlShortenerWebApp.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
